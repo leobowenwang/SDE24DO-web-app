4983f413cb43ba39fe3ef3b3b9e9472a16c014f2
Leo Bowen Wang <leobwang@gmail.com>
2023-11-04 17:41:46 +0100

    add security group

--- main.tf
+++ main.tf
@@ -1,21 +1,40 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
+resource "aws_security_group" "allow_web_traffic" {
+  name        = "allow_web_traffic"
+  description = "Allow web inbound traffic"
+  vpc_id      = "vpc-0e2232ea7e03046b9"
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  required_version = ">= 1.2.0"
-}
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
-provider "aws" {
-  region = "us-east-1"
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web_traffic"
+  }
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0e8a34246278c21e4"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_web_traffic.id]
 
   tags = {
     Name = "web-app"
